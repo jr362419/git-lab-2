ffa2cd8357df3c65f3a7821e0bedd3eba4ee145f
jr362419 <jr362419@ohio.edu>
2020-09-04 15:07:53 -0600

    the file git-lab-program.cc is working and will be pushed.

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Julian Renton
+ *        Date: 9/4/2020
+ * Description: Lab 2
+ * CS 2400, 112
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
